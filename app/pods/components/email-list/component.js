5e623ddac8618155dd9123f53768608d6a6ff2b9
Zied Ghalleb <zied.ghalleb@gmail.com>
2018-01-17 10:54:29 +0100

    Add total number of emails to the template

--- app/pods/components/email-list/component.js
+++ app/pods/components/email-list/component.js
@@ -1,18 +1,18 @@
-import Ember from 'ember';
-import { computed } from '@ember/object';
+import Ember from "ember";
+import { computed } from "@ember/object";
 
-const { inject : { service } } = Ember;
+const { inject: { service } } = Ember;
 export default Ember.Component.extend({
   invitations: service(),
 
-  content: computed.reads('invitations.content'),
-
+  content: computed.reads("invitations.content"),
+  
   actions: {
     deleteItem(invitation) {
-      this.get('invitations').delete(invitation);
+      this.get("invitations").delete(invitation);
     },
     updateItem(email, data) {
-      this.get('invitations').update(email, data);
+      this.get("invitations").update(email, data);
     }
   }
 });
